f660d4f51ec3b705365d0df5c859705cac1ecb30
suryamahato4444 <surya4444>
2021-02-15 09:29:38 +0545

    area

--- python_clss1.py
+++ python_clss1.py
@@ -2,3 +2,7 @@ length = float(input("Enter the value of length :"))
 breadth = float(input("Enter the value of breadth: "))
 area = 1/2 *(length * breadth)
 print("The area of given tringle is ",area,"cm square")
+l = float(input("Enter the value of length : "))
+Area = l**2
+print('The area of square is ',Area)
+
